4c708bab775f5688f41a4cd4e6af05c581cc3ba9
pabushabi <pabushabi@mail.ru>
2019-09-30 22:48:40 +0900

    inital commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Project Grass
+
+It`s 2D game constructor built with Vue and Electron
+
+### Project setup
+```
+npm install
+```
+### Run app
+```
+electron:serve
+```
+### Build app
+```
+electron:build
+```
+### Run your tests
+```
+test:unit
+```
+
+#### Run Vue app
+```
+vue-cli-service serve
+```
---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+    <h1>CONSTRUCTOR</h1>
+    <label>
+      label:
+      <input v-model="label" type="text" disabled>
+    </label>
+    <label>
+      title:
+      <input v-model="title" type="text">
+    </label>
+    <br>
+    <label>
+      speed:
+      <input v-model="speed" type="number">
+    </label>
+    <label>
+      gravity:
+      <input v-model="gravity" type="number">
+    </label>
+    <br>
+    <br>
+    <button @click="compile">compile</button>
+  </div>
+</template>
+
+<script>
+  const {app} = require('electron').remote;
+  const fs = require('fs');
+  console.log(app.getVersion());
+
+  export default {
+    name: 'app',
+    components: {},
+    data() {
+      return {
+        label: "",
+        title: "",
+        speed: 0.0,
+        gravity: 0.0,
+        newData: "",
+      }
+    },
+    methods: {
+      compile() {
+        console.log(`t-${this.title}, s-${this.speed}, g-${this.gravity}`);
+        fs.readFile('input/game.go', 'utf-8', (err, data) => {
+          if (err !== null) console.log(err);
+          console.log(err, data);
+          this.newData = data.replace(/gameTitle := "*([a-zA-Za-яА-Я0-9]|\s)*"*/gi, `gameTitle := "${this.title}"`);
+          this.newData = this.newData.replace(/gameGravity := (-|[a-zA-Za-яА-Я0-9]|\s)*\n/gi, `gameGravity := ${this.gravity}.0\n`);
+          this.newData = this.newData.replace(/gameRunSpeed := ([a-zA-Za-яА-Я0-9]|\s)*\n/gi, `gameRunSpeed := ${this.speed}.0\n\n`);
+          console.log(this.newData);
+          fs.writeFile('generated/game.go', this.newData, err1 => {
+            if (err1 !== null) console.log(err1);
+            const  execFile = require("child_process").execFile;
+            execFile('cmd', ['/c', `C:\\Go\\bin\\go build -i -o output/app.exe generated/game.go`], (err) => {
+              alert(`compiled! win-${process.getSystemVersion()}`);
+              console.log(`${err}`);
+              execFile('output/app.exe', (err, stdout, stderr) => {
+                console.log(`err-${err}, stderr-${stderr}`);
+              })
+            });
+          })
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin: 60px 10px 10px 10px;
+  }
+  input {
+    margin: 10px;
+  }
+</style>
